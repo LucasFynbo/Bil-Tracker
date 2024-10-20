15225fc2afdef2938d98762ac8f55f96a1448e59
Lucas Fynbo <lucasfynbo@gmail.com>
2024-10-20 21:11:32 +0200

    Rettelse af kode

--- src/tracker_esp.py
+++ src/tracker_esp.py
@@ -1,37 +1,35 @@
 from machine import I2C, Pin, UART
 import time
 import MPU6050
-import ConnectHandler
+import captive_portal
 import urequests
 import ujson as json
 
-class SendData:
-    def __init__(self, server_url, tracker_id=None):
+SERVER_URL = "http://79.171.148.143/api"
+TRACKER_ID = None
 
-        self.server_url = server_url
-        self.tracker_id = tracker_id
-
-    def send_data(self, type=None, gps=None):
+class HTTPServer:
+    def send_data(type=None, gps=None):
+        global TRACKER_ID
 
         if 'tracker id request' == type:
             try:
                 data_packet = {
-                    'data': 'tracker id request',					
+                    'data': 'tracker id request'				
                 }
 
                 print(f"[+] Sending data: {json.dumps(data_packet)}")
-                response = urequests.post(self.server_url, json=data_packet)
+                response = urequests.post(SERVER_URL, json=data_packet)
+
+                print(f"Server response: {response.text}")
 
                 response_data = response.json()
 
-                if response_data[0]['status'] == 'success':
-                    print(f"Server response: {response.text}")
-
-                    # Måske skal det være [0] eller [2] idk...
-                    self.tracker_id = response_data[2]['tracker_id']
+                if response_data['status'] == 'success':
+                    TRACKER_ID = response_data['tracker_id']
 
                     with open('tracker_id.txt', 'w') as file:
-                        file.write(f"TrackerID:{self.tracker_id}")
+                        file.write(f"TrackerID:{TRACKER_ID}")
 
                     print("[+] Tracker id request successfully handled")
 
@@ -43,21 +41,25 @@ class SendData:
 
 
         elif 'send gps coordinates' == type:
+
             try:                
                 data_packet = {
                     'data': 'received coords',	
-                    'coords': gps,
-                    'tracker_id': self.tracker_id				
+                    'coords_lat': f"{gps[0]}",
+                    'coords_long': f"{gps[1]}",
+                    'tracker_id': TRACKER_ID				
                 }
 
                 print(f"[+] Sending data: {json.dumps(data_packet)}")
-                response = urequests.post(self.server_url, json=data_packet)
+                response = urequests.post(SERVER_URL, json=data_packet)
 
             except Exception as e:
                 print(f"Unhandled exception: {e}")
 
 
 def tracker_id_control():
+    global TRACKER_ID
+    
     # Kontroller om der allerede er genereret et Tracker ID for enheden
     try:
         with open('tracker_id.txt', 'r') as file:
@@ -66,20 +68,19 @@ def tracker_id_control():
         # Hvis filen indeholdende tracker ID'et står tomt
         if '' == credentials_file:
             print("[!] Tracker ID file empty, requesting...")
-            SendData.send_data(type='tracker id request')
+            HTTPServer.send_data(type='tracker id request')
         # Hvis Tracker ID'et allerede er skabt for enheden
         else:
             for line in credentials_file.split('\n'):
                 if "TrackerID:" in line:
-                    tracker_id = line.split(':')[1].strip()
-            print(f"[+] Tracker ID: {tracker_id}")
-            return tracker_id
+                    TRACKER_ID = line.split(':')[1].strip()
+            print(f"[+] Tracker ID: {TRACKER_ID}")
             
     except OSError as e:
         # Hvis filen ikke eksisterer
         if errno.ENOENT == e.errno:
             print("[!] Tracker ID does not exist, requesting...")
-            SendData.send_data(type='tracker id request')
+            HTTPServer.send_data(type='tracker id request')
         # Andre errors
         else:
             print("[!] Error: '%s' occured." % e)
@@ -157,7 +158,6 @@ class Accelerometer:
         self.mpu.wake()
 
         self.gps = GPS()
-        self.send = SendData()
 
         # Flag to track if GPS data has been sent
         self.gps_data_sent = False
@@ -178,7 +178,7 @@ class Accelerometer:
 
                     if gps_data:
                         # Send GPS data
-                        self.send.send_data("send gps coordinates", gps_data)
+                        HTTPServer.send_data(type="send gps coordinates", gps=gps_data)
                         self.gps_data_sent = True  # Mark that data has been sent
 
                     # Start a grace period after sending GPS data
@@ -210,7 +210,7 @@ class Accelerometer:
                 gps_data = self.gps.read_gps()
 
                 if gps_data:
-                    self.send.send_data("send gps coordinates", gps_data)  # Send GPS data while acceleration > 0.0
+                    HTTPServer.send_data(type="send gps coordinates", gps=gps_data)  # Send GPS data while acceleration > 0.0
 
             else:
                 print("[!] Current acceleration not above threshold, waiting for next movement...")
@@ -218,12 +218,43 @@ class Accelerometer:
             time.sleep(0.5)  # Optional delay to avoid excessive processing
 
 if __name__ == "__main__":
-    connect = ConnectHandler.getip("BibNet", "")
-    print(f"Received IP: {connect}")
+    try:
+        with open('network_credentials.txt', 'r') as file:
+            credentials_file = file.read()
+            
+        # Hvis filen indeholdende network credentials står tomt
+        if '' == credentials_file:
+            print("[!] Network credentials file empty, please generate...")
+            ip_value = captive_portal.trackerConnection()
+        else:
+            for line in credentials_file.split('\n'):
+                if "SSID:" in line:
+                    ssid = line.split(':')[1].strip()
+                elif "PASS:" in line:
+                    password = line.split(':')[1].strip()
+
+            print(f"SSID: {ssid}, PASS: {password}")
+            ip_value = captive_portal.ConnectHandler.activate(ssid, password)
+            
+    except OSError as e:
+        # Hvis filen ikke eksisterer
+        if errno.ENOENT == e.errno:
+            print("[!] Network credentials doesn't exist, please generate...")
+            ip_value = captive_portal.trackerConnection()
+            
+        # Andre errors
+        else:
+            print("[!] Error: '%s' occured." % e)
+            ip_value = captive_portal.trackerConnection()
+    
+    print(f"Received IP: {ip_value}")
+    
+    tracker_id_control()
     
     time.sleep(2)
     
     MPU = Accelerometer()
     
     MPU.read()  # Start reading accelerometer data
+
 
